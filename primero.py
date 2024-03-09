40a82d99120a1bdeb31fc892dff31ffb98738e0e
Joseph Lari <joseph.larii@gmail.com>
2024-03-09 16:57:39 -0600

    Ya tiene un Frame ü

--- primero.py
+++ primero.py
@@ -1,1 +1,21 @@
 #Este es solo un programa de prueba para hacer la integracion de Git y Github
+
+import wx
+
+class MyFrame(wx.Frame):
+    def __init__(self,parent,title):
+        super(MyFrame, self).__init__(parent,title=title)
+
+
+
+class MyApp(wx.App):
+    def OnInit(self):
+        self.frame=MyFrame(parent=None, title="Creo que aqui va el titulo")
+        self.frame.Show()
+
+        return True
+
+app=MyApp()
+app.MainLoop()
+
+
